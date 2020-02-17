2616c6c220d46bf147caf5782ff4bcc2b8fcf790
Bryan de Ridder <br.deridder@gmail.com>
2020-02-17 21:58:11 +0100

    Removed babel

--- webpack.config.js
+++ webpack.config.js
@@ -10,13 +10,6 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
         test: /\.tsx?$/,
         exclude: /node_modules/,
         use: [
